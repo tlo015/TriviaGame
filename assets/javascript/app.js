8137e32e433569cca752b44ea8f3bdc25931aad6
Regina Lo <35210714+tlo015@users.noreply.github.com>
2018-03-26 16:56:16 -0700

    add all components

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,38 +2,59 @@
 //var containing all the questions 
 var quizQuestions = [
     {
-        question: "What is 1+1?",
-        answers: ["one", "two", "three", "four"],
-        // a: "one",
-        // b: "two", 
-        // c: "three",
-        // d: "four",
-        correct: "two",            
+        question: "How did Viserys Targaryen die?",
+        answers: ["Stabbed in the heart", "With a melted golden crown", "Beheading", "Burned alive"],
+        correct: "With a melted golden crown",            
     },
     {
-        question: "What is 1x1?",
-        answers: ["one", "two", "three", "four"],
-        // a: "one",
-        // b: "two", 
-        // c: "three",
-        // d: "four",
-        correct: "one",
+        question: "Who created the White Walkers?",
+        answers: ["The First Men", "The Three-Eyed Raven", "The Wildlings", "The Children Of The Forest"],
+        correct: "The Children Of The Forest",
     },
     {
-        question: "What is 1+2?",
-        answers: ["one", "two", "three", "four"],
-        // a: "one",
-        // b: "two", 
-        // c: "three",
-        // d: "four",
-        correct: "three",
-    }
+        question: "What is the only thing that can put out volatile Wildfire?",
+        answers: ["Sand", "Water", "Dragon's blood", "Sunlight"],
+        correct: "Sand",
+    },
+    {
+        question: "The Night King was created using a dagger made of:",
+        answers: ["Blue Ice", "Obsidian", "Valyrian Steel", "Dragonglass"],
+        correct: "Dragonglass",
+    },
+    {
+        question: "How many times has Beric Dondarrion been brought back to life?",
+        answers: ["Three times", "Five Times", "Six TImes", "Seven TImes"],
+        correct: "Six Times",
+    },
+    {
+        question: "Which Stark family direwolf was killed in retaliation for an attack on Prince Joffrey?",
+        answers: ["Ghost", "Lady", "Nymeria", "Summer"],
+        correct: "Lady",
+    },
+    {
+        question: "What was the name of Ned Stark's greatsword?",
+        answers: ["Ice", "Northguard", "Oathkeeper", "Widow's Wail"],
+        correct: "Ice",
+    },
+    {
+        question: "Prince Oberyn Martell is nicknamed the 'Red Viper' because of his combat and:",
+        answers: ["Pride in drawing first blood", " Knowledge in poisons", "Nighttime attacks", "Ruby-colored armor"],
+        correct: "Knowledge of poisons",
+    },
+    {
+        question: "How many arrows does Ramsay Bolton let loose at Rickon Stark?",
+        answers: ["Three", "Five", "Two", "Four"],
+        correct: "Four",
+    },
+    {
+        question: "In which territory is Mance Rayder recognized as king?",
+        answers: ["King's Landing", "Westeros", "House Tyrell", "Beyond the Wall"],
+        correct: "Beyond the Wall",
+    },
+        
 ];
 
-//VARIABLES
-//variables to display when game starts, hold the questions and answers 
-var seconds = 5;
-
+//----------VARIABLES----------
 //variable to display when timer reaches 0
 var correct = 0;
 var incorrect = 0;
@@ -53,6 +74,8 @@ $(".start").on("click", function() {
 
 //start timer
 function startTimer() {
+    //variables to display when game starts, hold the questions and answers 
+    seconds = 5;
     intervalId = setInterval (decrement,500);
 }
 
@@ -61,26 +84,33 @@ function startTimer() {
 function decrement() {
     seconds --;
     $("#time-left").html("<p>Time remaining: " + seconds + " secs </p>")
+    if (seconds < 0) {
+        stop()
+        showAnswer();
+    }
 }
 
 // function seconds=0, show results, ready for next question 
 function stop() {
     clearInterval(intervalId);  
+    $("#time-left").hide();
+    $("#game-question").hide();
+    $("#game-answers").hide();
 }
 
 //--------- QUESTION AND ANSWER SLIDE ----------
-//function to display the question (11-slidehow)
+//function to display the question 
 //display choices onto the page, under each question 
 var counter=0;
 function showQuestion() {
     $("#game-question").html("<p>" + quizQuestions[counter].question + "</p>")
     console.log (quizQuestions[counter].question)
-}
 
     for (var i=0; i<4; i++){
         var choices = $("<button>");
         choices.text(quizQuestions[counter].answers[i]);
-        choices.attr("index", i);
+        //set an attribute referencing index position within the array
+        choices.attr({"data-index":i});
         choices.addClass("select")
         $("#game-answers").append(choices);
     }
@@ -88,18 +118,22 @@ function showQuestion() {
 //clicking on button to stop timer 
 //show answer page
     $("#game-answers").on("click", function(){
-        userChoice = $('.select').data("index")
+        userChoice = $(this).data("index")
+        console.log (userChoice);
         clearInterval(intervalId);
         stop();
-            $("#time-left").hide();
-            $("#game-question").hide();
-            $("#game-answers").hide();
         showAnswer();
     })
+}
+
 
 //---------WHETHER CLICK WAS CORRECT OR INCORRECT ----------
-//if time=0 or onclickfunvtion
+// when timer = 0secs or when done button is clicked 
+// returns the correct, incorrect and unanswered counts to the page 
 function showAnswer() {
+    $("#time-left").hide();
+    $("#game-question").hide();
+    $("#game-answers").hide();
     var rightAns = quizQuestions[counter].correct;
     if (userChoice == rightAns) {
         correct++;
@@ -115,36 +149,35 @@ function showAnswer() {
     }
 }
 
-
-
-
-
-
-
-})
-
-
-
-//start newtimer 
-
+//start newtimer (11-slidehow)
 //stop newtimer
 //counter++
-//hide
- 
+//hide and display next question
+function nextQuestion() {
+    counter++;
+    setTimeout(showQuestion, 5000);
+    if (counter === quizQuestions.length){
+        showResults();
+    }
+}
 
-// showQuestion();
-
-//counter = .length
-//total results 
-//restrt button 
-
-        
-    
 // function that stores the results 
 // results 
+function showResults() {
+    $("#time-left").hide();
+    $("#game-question").hide();
+    $("#game-answers").hide();
 
-// when timer = 0secs or when done button is clicked 
-// returns the correct, incorrect and unanswered counts to the page 
+    $(".ansCorrect").html("<p> Correct Answers: " + correct + "</p>");
+    $(".ansIncorrect").html("<p> Incorrect Answers: " + incorrect + "</p>");
+    $(".ansNone").html("<p> Unanswered Quesions: " + unanswered + "</p>");
+}
 
+$(".restart").on("click", function() {
+    $(".restart").hide();
+    showResults().hide();
+    startTimer();
+    showQuestion();
+})
 
-
+})
